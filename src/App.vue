1d755bcdad7036532a6566d1f31f91ffa60e340c
RainbowEclipsee <dexterrullez1@gmail.com>
2022-08-01 23:22:16 +0300

    main

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Todo List</h1>
+    <hr />
+    <TodoList v-bind:todos="todos" 
+    @remove-todo='removeTodo'/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList,
+  },
+  data() {
+    return {
+      todos:[
+        {id: 1, title: 'Купить вещь1', completed: false},
+        {id: 2, title: 'Купить вещь2', completed: false},
+        {id: 3, title: 'Купить вещь3', completed: false},
+      ]
+    }
+  },
+  methods:{
+    removeTodo(id){
+      this.todos = this.todos.filter(t => t.id !== id)
+    }
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul>
+      <TodoItem 
+      v-for="todo in todos" 
+      v-bind:todo='todo' 
+      :key="todo"
+      v-on:remove-todo='removeTodo'/>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+  props: ['todos'],
+  components: {
+    TodoItem,
+  },
+  methods:{
+    removeTodo(id){
+      this.$emit('remove-todo', id)
+    }
+  },
+}
+</script>
+
+<style>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }</style>
